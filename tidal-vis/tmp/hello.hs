6989e6133b9c7adb59459a805ad5b655809c6d29
alex <alex@slab.org>
2023-07-27 10:22:01 +0100

    minimal viable turtle

--- tidal-vis/tmp/hello.hs
+++ tidal-vis/tmp/hello.hs
@@ -6,27 +6,39 @@ import           Control.Concurrent
 import           Control.Concurrent.MVar
 import           Graphics.Gloss
 import           Graphics.Gloss.Interface.IO.Animate
+import qualified Sound.Tidal.Context                 as T
 
-makeThread :: IO (MVar String)
-makeThread = do myVariable <- newMVar "Hello World!!!"
+data Turtle = L Float
+            | R Float
+            | F Float
+
+makeThread :: IO (MVar (T.Pattern Turtle))
+makeThread = do mvPattern <- newMVar $ T.fastcat $ map pure [F 10, R 45, F 20, L 90, F 10]
                 putStr "a"
-                threadId <- forkIO $ myAnimate myVariable
+                threadId <- forkIO $ myAnimate mvPattern
                 putStr "b"
-                return myVariable
+                return mvPattern
 
-myAnimate myVariable =
+myAnimate mvPattern =
   animateIO
         (InWindow
-               "Hello World!!"     -- window title
+               "Hello World!!"   -- window title
                 (400, 150)       -- window size
                 (10, 10))        -- window position
         white                    -- background color
-        (picture myVariable)     -- picture to display
+        (picture mvPattern)     -- picture to display
         controllerSetRedraw
 
-picture :: MVar String -> Float -> IO Picture
-picture myVariable t =
-  do str <- readMVar myVariable
-     return $ Translate (-170) (-20) -- shift the text to the middle of the window
-       $ Scale 0.5 0.5               -- display it half the original size
-       $ Text (str ++ " " ++ show t)
+drawLines :: [Turtle] -> Picture
+drawLines []         = mempty
+drawLines ((L angle):ts) = rotate angle $ drawLines ts
+drawLines ((R angle):ts) = rotate (0 - angle) $ drawLines ts
+drawLines ((F len):ts) = translate len 0 $ pictures [line [(0,0), (-len,0)], drawLines ts]
+
+picture :: MVar (T.Pattern Turtle) -> Float -> IO Picture
+picture mvPattern t =
+  do pat <- readMVar mvPattern
+     let values = map T.value $ T.queryArc pat (T.Arc (toRational t) (toRational $ t + 4))
+     return -- Translate (-170) (-20) -- shift the text to the middle of the window
+       -- $ Scale 0.5 0.5               -- display it half the original size
+       $ drawLines values
